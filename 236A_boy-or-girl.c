2c8aea0436a3ece079ba628a23308bf2cb1ed6c0
Radimir007 <radovan.psotny123@gmail.com>
2025-11-04 19:40:35 +0100

    236A submitted

--- 236A_boy-or-girl.c
+++ 236A_boy-or-girl.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-int main () {
-
-    char input[100], distincts[100];
-
+int main()
+{
+    char input[100];
     scanf("%s", input);
 
-    for (int i = 0; i < strlen(input); i++) {
-        if (i == 0) {
-            distincts[0] = input[i];
-            continue;
-        }
+    int count = 1;
 
-        bool isDistinct = true;
-        for (int j = 0; j < strlen(distincts); j++) {
-            if (input[i] == distincts[j]) {
-                isDistinct = false;
-            }
-
-            if (j == strlen(distincts) - 1 && isDistinct) {
-                distincts[strlen(distincts)] = input[i];
-            }
+    for (int i = 1; i < strlen(input); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (input[i] == input[j])
+                break;
             
+            if (j >= i - 1)
+                count++;
         }
     }
 
-    if (strlen(distincts) % 2 == 0) {
-        printf("%s", "CHAT WITH HER!\n");
-    } else {
-        printf("%s", "IGNORE HIM!\n");
-    }
+    count % 2 ? printf("IGNORE HIM!\n") : printf("CHAT WITH HER!\n");
 
     return 0;
 }
